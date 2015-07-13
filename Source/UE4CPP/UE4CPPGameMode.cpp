61985a53ee1e2917cfa970f6ccdaf744b72475f1
PaulGap <paulgschwendtner@me.com>
2015-07-13 19:59:15 +0200

    Rename source subfolder

--- Source/UE4CPP/UE4CPPGameMode.cpp
+++ Source/UE4CPP/UE4CPPGameMode.cpp
@@ -9,7 +9,7 @@ AUE4CPPGameMode::AUE4CPPGameMode(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/UE4CPP/Blueprints/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
